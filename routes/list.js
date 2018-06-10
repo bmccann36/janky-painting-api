d5d1fda7e0039dc8d59730363edc19ffb5ea2f71
Brian McCann <bmccann36@gmail.com>
2018-06-10 16:31:47 -0400

    hooked up our function with end point and database

--- routes/list.js
+++ routes/list.js
@@ -1,23 +1,51 @@
 'use strict';
 
 const chalk = require('chalk')
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const readYaml = require('read-yaml');
+
+let localEnv
+try { localEnv = readYaml.sync('env.yml').local; }
+catch (e) { console.log('env not local') }
+const environment = localEnv ? localEnv : process.env
+
+
 
 module.exports.main = (event, context, callback) => {
 
   console.log('test variable: ', chalk.magenta(process.env.TEST_VAR))
 
-  console.log('arbitrary change')
-
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+  var params = {
+    TableName: environment.POSTS_TABLE,
   };
 
-  callback(null, response);
+  dynamoDb.scan(params).promise()
+    .then(res => {
+      // format success response
+      callback(null, buildResponse(200, res.Items))
+    })
+    .catch(err => {
+      // format error response
+      callback(null, buildResponse(500, err))
+    })
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+// module.exports.main({}, null, (err, scc) => {
+//   if (err) console.log(err)
+//   else console.log(scc)
+// })
+
+
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true
+    },
+    body: JSON.stringify(body)
+  };
+}
